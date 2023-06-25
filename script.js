de612e41dfcadc689395bd2fdb856b2fa95144c7
vinceFaure <faurvincent@gmail.com>
2023-06-25 15:08:41 +0200

    important update

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 "use strict";
 
 
+/* Sidebar */
 
+let sidebar = document.querySelector('#sideBar');
+let sidebarToggler = document.querySelector('.sideBarToggler');
+
+
+// Toggling the Sidebar
+sidebarToggler.addEventListener('click', () => {
+    sidebar.classList.toggle('show');
+});
+
+
+// Closing the Sidebar on clicking Outside and on the Sidebar-Links
+window.addEventListener('click', (e) => {
+    if (e.target.id !== 'sideBar' && e.target.className !== 'sideBarToggler') {
+        sidebar.classList.remove('show');
+    }
+});
 
 
 
