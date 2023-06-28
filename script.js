1c9bad4c3e6b11678978450bfe4751ceba6a2ed3
vinceFaure <faurvincent@gmail.com>
2023-06-28 12:32:12 +0200

    routin

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 "use strict";
 
 
-/* Sidebar */
+/* sideBar */
 
-let sidebar = document.querySelector('#sideBar');
-let sidebarToggler = document.querySelector('.sideBarToggler');
+let sideBar = document.querySelector('#sideBar');
+let sideBarToggler = document.querySelector('.menu');
 
 
-// Toggling the Sidebar
-sidebarToggler.addEventListener('click', () => {
-    sidebar.classList.toggle('show');
+// Toggling the sideBar
+
+sideBarToggler.addEventListener('click', () => {
+    sideBar.classList.toggle('show');
+
+    sideBarToggler.classList.toggle('opened')
+    sideBarToggler.setAttribute('aria-expanded',sideBarToggler.classList.contains('opened')); 
+    sideBarToggler.classList.contains('opened')
+    
 });
 
 
-// Closing the Sidebar on clicking Outside and on the Sidebar-Links
+// Closing the sideBar on clicking Outside and on the sideBar-Links
 window.addEventListener('click', (e) => {
-    if (e.target.id !== 'sideBar' && e.target.className !== 'sideBarToggler') {
-        sidebar.classList.remove('show');
+    if (sideBar.classList.contains('show') !== true) {
+     console.log('Sidebar clicked out')
+     
+     
     }
 });
 
