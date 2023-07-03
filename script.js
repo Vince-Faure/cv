1fb418026bd0235ad001e949e3c903c1853e1290
vinceFaure <faurvincent@gmail.com>
2023-07-03 14:07:58 +0200

    Cool editz

--- script.js
+++ script.js
@@ -21,8 +21,10 @@ sideBarToggler.addEventListener('click', () => {
 
 // Closing the sideBar on clicking Outside and on the sideBar-Links
 window.addEventListener('click', (e) => {
-    if (sideBar.classList.contains('show') !== true) {
+    if (sideBar.classList.contains('show') && !sideBarToggler.contains(e.target) ) {
      console.log('Sidebar clicked out')
+     sideBar.classList.remove('show');
+     sideBarToggler.classList.remove('opened')
      
      
     }
